b89c8e55d2052d0d8953d5e2eac007e27ec5a768
swayyDev <94021460+swayyDev@users.noreply.github.com>
2022-02-01 15:14:27 -0800

    Add files via upload

---
+++ wig.php
@@ -0,0 +1,637 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Glamour|homepage</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrapcss.css">
+	<link rel="stylesheet" type="text/css" href="wig.css">
+    <script type="text/javascript" src="JQuery-3.6.0.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Open+Sans:wght@400;500;600;700;800&family=Poppins:wght@600;700;800&family=Raleway:wght@600;700;800;900&display=swap" rel="stylesheet">
+<ion-icon src="/path/to/external/file.svg"></ion-icon>
+</head>
+<body>
+	<header>
+	<div class="wrapper">
+		<div class="logo">
+			<a href="#">Gla<span>mour</span></a>
+		</div>
+		<form action="">
+		<span class="fa fa-search"></span>
+		<input type="text" placeholder="search store">
+		</form>
+		<div class="a-btn">
+			<a href="#">manufacturers</a>
+            <a href="#">packaging</a>
+			<a href="#">story</a>
+		</div>
+		<ul>
+			<li>
+				<a href="#">
+					<span style="text-decoration: none;">2</span>
+				<i class="far fa-heart" style="text-decoration: none; outline:none; ">&nbsp;</i>&nbsp;</a>
+			</li>&nbsp;		
+			<li>
+			<a href="#">
+		    <ion-icon name="cart-outline"></ion-icon></a>
+		    </li>
+		</ul>
+
+     		
+	</div>
+
+	</header>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="slider">
+		<div class="slider-heading">
+			<h2>Explore</h2>
+		</div>
+		<div class="slider-items">
+			<h4><img src="pictures/headphone.png" alt="">&nbsp;<a href="#">new in</a></h4><br>			
+			<h4><img src="pictures/shirt.png" alt="">&nbsp;<a href="#">clothing</a></h4><br>
+			<h4><img src="pictures/sneakers.png" alt="">&nbsp;<a href="#">shoes</a></h4><br>
+			<h4><img src="pictures/watch.png" alt="">&nbsp;<a href="#">accessories</a></h4><br>
+			<h4><img src="pictures/gift.png" alt="">&nbsp;<a href="#">gifts</a></h4><br>
+    		<h4><img src="pictures/gym.png" alt="">&nbsp;<a href="#">activewears</a></h4><br>
+    		<h4><img src="pictures/diamond.png" alt="">&nbsp;<a href="#">inspirations</a></h4><br>
+		</div>	
+ 	</div>
+</div>
+	<div class="col-md-6 ranger">
+	<div id="carouselBlog" class="carousel slide" data-ride="carousel">
+		
+		<ol class="carousel-indicators">
+			<li data-target="#carouselBlog" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselBlog" data-slide-to="1"></li>
+		</ol>
+
+		<div class="carousel-inner">
+
+			<div class="carousel-item active" interval="100">
+				<img src="pictures/beanie.jpg" class="w-100">
+				<div class="carousel-caption">
+           <h3>Enjoy this <br>summer trends</h3>
+          <p>order eligible items of $200 and more</p>
+					<button><ion-icon name="cart-outline"></ion-icon><a href="#">Shop Now</a></button>
+				</div>
+			</div>
+
+
+			<div class="carousel-item" interval="100">
+				<img src="pictures/sharp.jpg" class="w-100">
+				<div class="carousel-caption">
+          <h3>Enjoy this <br>summer trends</h3>
+          <p>order eligible items of $200 and more</p>
+					<button><ion-icon name="cart-outline"></ion-icon></a><a href="#">Shop Now</a></button>
+				</div>
+
+			</div>
+			
+		</div>
+
+
+		<a href="#carouselBlog" class="carousel-control-prev" data-slide="prev" role="button">
+			<span class="carousel-control-prev-icon"></span>
+		</a>
+		<a href="#carouselBlog" class="carousel-control-next" data-slide="next" role="button">
+			<span class="carousel-control-next-icon"></span>
+		</a>
+		<!-- <a href="#carouselBlog" class="carousel-control-next" data-slide="next" role="button">
+			<span class="carousel-control-next-icon"></span>
+		</a>
+ -->
+
+		 
+	</div>
+		</div>
+		
+		<div class="col-md-3 trending">
+			<h5>Trending for you<ion-icon name="flame"></ion-icon></h5>          	
+           	 <div id="carouselBlog" class="carousel slide" data-ride="carousel">
+		
+		<ol class="carousel-indicators">
+			<li data-target="#carouselBlog" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselBlog" data-slide-to="1"></li>
+		</ol>
+
+		<div class="carousel-inner">
+
+			<div class="carousel-item active" interval="100">
+				<img src="pictures/oshia.png" class="w-100">
+			</div>
+
+
+			<div class="carousel-item" interval="100">
+				<img src="pictures/nik.jpg" class="w-100">
+			</div>
+
+		
+		</div>
+		</div>
+		<h4>Version</h4>
+			<button><input type="radio" name="">Air Jordan 5 Retro</button><br>
+			<button>
+		    <ion-icon name="cart-outline"></ion-icon>Check Out</button>
+		
+	</div>
+
+
+
+ <div class="container-fluid">
+ 		<div class="row">
+ 		  <div class="col-md-1"></div>
+ 		  <div class="col-md-11 category" id="men-pic">
+ 		 	<div class="categoryheading">
+ 		 		<h3>Fashion Category</h3>
+ 		 	</div>
+
+
+ 		 	<div class="a-btn">
+ 		 		<button id="men-pic" style="background-color: #C1C9FF; color: #fff;">Men</button>
+ 		 		<button id="showbuttonwomen">Women</button>
+ 		 		<button id="showbuttonshoes">Shoes</button>
+ 		        <button>Accessories</button>
+                <span><a href="#">see all</a></span>
+ 		 	</div>
+ 		 	
+
+ 		 	<div class="row men-pic" >
+ 		 		<div class="img-container">
+ 		 			<img src="pictures/sharp.jpg">
+ 		 			<h1>plain cardigan <br> $400</h1>
+ 		 			<div class="icons">
+ 		 				<span class="fas fa-heart" style="color: red;"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 		    </div>
+ 		 	    </div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/dry.webp">
+                  <h1>oversized organic clothes <br>$336</h1>
+ 		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/jacket.jpg">
+  		 			<h1>cologne Jacket<br>$336</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/wet.webp">
+  		 			<h1>Homme plisse issey miyake men<br>$449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		 
+  		 		 <div class="img-container">
+  		 			<img src="pictures/gucci.jpg">
+  		 			<h1>Gucci Full clothing<br>$99</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>	
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/suit.jpg">
+  		 			 <h1>Homme Made Full Cologne Suit<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	        </div>  
+  		 		</div>
+
+
+                   <div class="img-container">
+  		 			<img src="pictures/jacket1.jpg">
+  		 			 <h1>corduroy shirt<br>$149</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+ 		 	       <div class="img-container">
+  		 			<img src="pictures/fine.jpg">
+  		 			 <h1>joggers and sweatshirt<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+
+
+ 		 	          <div class="img-container">
+  		 			<img src="pictures/black.jpg">
+  		 			 <h1>thick leather Jacket<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+ 		 	      <div class="img-container">
+  		 			<img src="pictures/air.jpg">
+  		 			 <h1>orignal air jordan<br>$500</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div>
+                 <div class="img-container">
+  		 			<img src="pictures/nik.jpg">
+  		 			 <h1>corduroy shirt<br>$300</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div> 	
+
+ 		 	<div class="img-container">
+  		 			<img src="pictures/ni.jpg">
+  		 			 <h1>Nike Revolt<br>$489</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div> 		 	
+	 	
+
+
+        </div>
+ 	</div>
+ </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container-fluid">
+ 		<div class="row">
+ 		  <div class="col-md-1"></div>
+ 		  <div class="col-md-11 category-women" id="Women-pic">
+
+            <div class="categoryheading">
+ 		 		<h3>Fashion Category</h3>
+ 		 	</div>
+
+ 		  	<div class="a-btn">
+ 		 		<button id="showbutton">Men</button>
+ 		 		<button id="showbuttonwomen">Women</button>
+ 		 		<button id="showbuttonshoes">Shoes</button>
+ 		        <button>Accessories</button>
+                <span><a href="#">see all</a></span>
+ 		 	</div>
+
+ 		 	
+
+ 		 	<div class="row Women-pic">
+ 		 		<div class="img-container">
+ 		 			<img src="pictures/women1.jpg">
+ 		 			<h1>plain cardigan <br> $400</h1>
+ 		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 		    </div>
+ 		 	    </div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/women2.jpg">
+                  <h1>oversized organic clothes <br>$336</h1>
+ 		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/women3.jpg">
+  		 			<h1>cologne Jacket<br>$336</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/women4.jpg">
+  		 			<h1>Homme plisse issey miyake men<br>$449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		 
+  		 		 <div class="img-container">
+  		 			<img src="pictures/women5.jpg">
+  		 			<h1>Gucci Full clothing<br>$99</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>	
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/women6.jpg">
+  		 			 <h1>Homme Made Full Cologne Suit<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	        </div>  
+  		 		</div>
+
+
+                   <div class="img-container">
+  		 			<img src="pictures/women7.jpg">
+  		 			 <h1>corduroy shirt<br>$149</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+ 		 	       <div class="img-container">
+  		 			<img src="pictures/women8.jpg">
+  		 			 <h1>joggers and sweatshirt<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+
+
+ 		 	          <div class="img-container">
+  		 			<img src="pictures/women9.jpg">
+  		 			 <h1>thick leather Jacket<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+ 		 	      <div class="img-container">
+  		 			<img src="pictures/women10.jpg">
+  		 			 <h1>orignal air jordan<br>$500</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div>
+                 <div class="img-container">
+  		 			<img src="pictures/women11.jpg">
+  		 			 <h1>corduroy shirt<br>$300</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div> 	
+
+ 		 	<div class="img-container">
+  		 			<img src="pictures/women12.jpg">
+  		 			 <h1>Nike Revolt<br>$489</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div> 		 	
+	 	
+
+
+        </div>
+ 	</div>
+ </div>
+
+
+
+
+
+
+
+
+
+ <div class="container-fluid">
+ 		<div class="row">
+ 		  <div class="col-md-1"></div>
+ 		  <div class="col-md-11 category-shoe" id="shoe-pic">
+
+            <div class="categoryheading">
+ 		 		<h3>Fashion Category</h3>
+ 		 	</div>
+
+ 		  	<div class="a-btn">
+ 		 		<button id="showbutton">Men</button>
+ 		 		<button id="showbuttonwomen">Women</button>
+ 		 		<button id="showbuttonshoes">Shoes</button>
+ 		        <button>Accessories</button>
+                <span><a href="#">see all</a></span>
+ 		 	</div>
+
+ 		 	
+
+ 		 	<div class="row shoe-pic">
+ 		 		<div class="img-container">
+ 		 			<img src="pictures/shoe1.png">
+ 		 			<h1>plain cardigan <br> $400</h1>
+ 		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 		    </div>
+ 		 	    </div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/shoe2.png">
+                  <h1>oversized organic clothes <br>$336</h1>
+ 		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/shoe3.png">
+  		 			<h1>cologne Jacket<br>$336</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/shoe4.png">
+  		 			<h1>Homme plisse issey miyake men<br>$449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>
+  		 		 
+  		 		 <div class="img-container">
+  		 			<img src="pictures/shoe5.png">
+  		 			<h1>Gucci Full clothing<br>$99</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 			</div>
+  		 		</div>	
+  		 		
+  		 		<div class="img-container">
+  		 			<img src="pictures/shoe6.png">
+  		 			 <h1>Homme Made Full Cologne Suit<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	        </div>  
+  		 		</div>
+
+
+                   <div class="img-container">
+  		 			<img src="pictures/shoe7.png">
+  		 			 <h1>corduroy shirt<br>$149</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+ 		 	       <div class="img-container">
+  		 			<img src="pictures/shoe8.png">
+  		 			 <h1>joggers and sweatshirt<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+
+
+ 		 	          <div class="img-container">
+  		 			<img src="pictures/shoe9.png">
+  		 			 <h1>thick leather Jacket<br>$1449</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	      </div>
+
+ 		 	      <div class="img-container">
+  		 			<img src="pictures/women10.jpg">
+  		 			 <h1>orignal air jordan<br>$500</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div>
+                 <div class="img-container">
+  		 			<img src="pictures/women11.jpg">
+  		 			 <h1>corduroy shirt<br>$300</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div> 	
+
+ 		 	<div class="img-container">
+  		 			<img src="pictures/women12.jpg">
+  		 			 <h1>Nike Revolt<br>$489</h1>
+  		 			<div class="icons">
+ 		 				<span class="far fa-heart"></span>
+ 		 				<button><ion-icon name="bag-outline"></ion-icon>Add to bag</button>
+ 		 	          </div>
+ 		 	</div> 		 	
+	 	
+
+
+        </div>
+ 	</div>
+ </div>
+
+ <div class="container">
+  <div class="bottom-footer">
+    <div class="row">
+      <div class="col-md-11 text-center">
+        <h3>Stay In Touched</h3>
+        <p>Get briefings from our shop experts</p>
+        <button><a href="contact.php">contact us</a></button>
+        </div>
+        <div class="col-md-1"></div>
+  </div>
+    </div>
+ </div>
+ <footer>
+   <h3>designed by @swayyDev</h3>
+ </footer>
+
+
+
+ <script type="text/javascript">
+ 	$(function(){
+ 		$("#showbutton").click(function(){
+ 			$("#men-pic").show();
+ 			$("#Women-pic").hide();
+
+ 		})
+ 		$("#showbuttonwomen").click(function(){
+ 			$("#Women-pic").show();
+ 			$("#men-pic").hide();
+ 		})
+ 		$("#showbuttonshoes").click(function(){
+ 			$("#shoe-pic").show();
+ 			$("#men-pic").hide();
+ 			$("#Women-pic").hide();
+ 		})
+ 	})
+
+
+ </script>
+
+
+
+
+
+
+
+<ion-icon src="/path/to/external/file.svg"></ion-icon>
+
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+
+</body>
+</html>	
